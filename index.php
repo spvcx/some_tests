7ce1363fdf4b22ad1022eb2e5ffa39499857208b
spvcx <spvcxxa@gmail.com>
2018-01-12 15:51:35 +0300

    some php oop

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
-echo "phptest";
+interface Product {
+    public function getName();
+}
+class Factory {
+    protected $firstProduct;
+    protected $secondProduct;
+
+    public function getFirstProduct() {
+        if(!$this->firstProduct) {
+            $this->firstProduct = new FirstProduct();
+        }
+        return $this->firstProduct;
+    }
+
+    public function getSecondProduct() {
+        if(!$this->secondProduct) {
+            $this->secondProduct = new SecondProduct();
+        }
+        return $this->secondProduct;
+    }
+
+}
+
+class FirstProduct implements Product {
+    public function getName(){
+        return "First Product Name";
+    }
+}
+class SecondProduct implements Product {
+    public function getName(){
+        return "Second Product Name";
+    }
+}
+
+$factory = new Factory();
+print_r($factory->getFirstProduct()->getName());
+print_R($factory->getSecondProduct()->getName());
 ?>
